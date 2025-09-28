0f918c31db2d54151b2d0d7e51c8c3d7659e0216
David Gomez <davgomez123t@gmail.com>
2025-09-28 19:57:34 -0300

    rankings, comentarios
    agregar rankings, comentarios, etc

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,6 +132,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
 
 from django.contrib.messages import constants as messages
 
